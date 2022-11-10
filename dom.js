4339468fa1fcb1c7d1d79e28f5951c0b6aade396
Emmanuel <girault.e@gmail.com>
2022-11-10 22:53:57 +0100

    Added UI and board mechanics

--- dom.js
+++ dom.js
@@ -33,14 +33,18 @@ function introStart() {
     document.body.appendChild(inputWrapper);
 }
 let player1Board;
+let player2Board;
 function gameStart() {
     const name1=document.querySelector('.name1Input').value;
     const name2=document.querySelector('.name2Input').value;
     const inputWrapper = document.querySelector('.inputWrapper');
     inputWrapper.remove();
     //Initialize boards
-    player1Board = Gameboard();
-    const player2Board = Gameboard();
+    const currentGame = Game(name1, name2);
+    // player1Board = Gameboard();
+    // player2Board = Gameboard(true);
+    player1Board = currentGame.p1board;
+    player2Board = currentGame.p2board;
     //Display boards
     const board1 = document.createElement('div');
     board1.classList.add('board1');
@@ -49,18 +53,17 @@ function gameStart() {
     for(let i=10;i>=1;i--) {
         for(let j=1;j<=10;j++) {
             const board1Cell = document.createElement('div');
-            board1Cell.classList.add('board1Cell', 'boardCell');
+            board1Cell.classList.add('board1Cell', 'boardCell', 'boardCellRelative');
             const board2Cell = document.createElement('div');
-            board2Cell.classList.add('board2Cell', 'boardCell');
+            board2Cell.classList.add('board2Cell', 'boardCell', 'boardCellRelative');
             board1.appendChild(board1Cell);
             board2.appendChild(board2Cell);
             board1Cell.setAttribute('data-posX',`${j}`);
             board1Cell.setAttribute('data-posY',`${i}`);
-            board2Cell.setAttribute('data-posX',`${j}`);
-            board2Cell.setAttribute('data-posY',`${i}`);
+            board2Cell.setAttribute('data-aiX',`${j}`);
+            board2Cell.setAttribute('data-aiY',`${i}`);
             board1.appendChild(board1Cell);
             board2.appendChild(board2Cell);
-            // board1Cell.addEventListener('dragstart', dragStart);
         }
     }
 
@@ -76,34 +79,32 @@ function gameStart() {
     function initShips() {
         const shipsToPlaceWrapper = document.createElement('div');
         shipsToPlaceWrapper.classList.add('shipsToPlaceWrapper');
-        let toPlace=(player1Board.shipsToPlace.length);
+        let toPlace=(currentGame.p1shipsToPlace.length);
         for(i=0;i<toPlace;i++) {
             const placeShip = document.createElement('div');
-            placeShip.classList.add('placeShip', `ship${i+1}`);
+            placeShip.classList.add('placeShip', `ship${currentGame.p1shipsToPlace[i]}`, 'placeShipHover');
             placeShip.setAttribute('draggable','true');
             placeShip.id=`id${i+1}`;
-            placeShip.setAttribute('data-size',`${i+1}`);
+            placeShip.setAttribute('data-size',`${currentGame.p1shipsToPlace[i]}`);
             const shipImg = document.createElement('img');
             shipImg.classList.add(`shipImg`);
-            shipImg.src=`./assets/Ship${i+1}.png`;
+            shipImg.src=`./assets/Ship${currentGame.p1shipsToPlace[i]}.png`;
             placeShip.appendChild(shipImg);
             shipsToPlaceWrapper.appendChild(placeShip);
             placeShip.addEventListener('dragstart', dragStart);
-            placeShip.addEventListener('dragend', dragEnd);
             placeShip.addEventListener('click', rotate);
         }
         document.body.appendChild(shipsToPlaceWrapper);
     }
 
     //Drag option
-    board1.addEventListener('dragenter', dragEnter);
-    board1.addEventListener('dragover', dragOver);
+    board1.addEventListener('dragenter', dragFunction);
+    board1.addEventListener('dragover', dragFunction);
     board1.addEventListener('dragleave', dragLeave);
     board1.addEventListener('drop', drop);
     let elementDragged=undefined;
 
     function dragStart(e) {
-        // e.dataTransfer.setData('text/plain', e.currentTarget.id);
         elementDragged = e.currentTarget;     
     }
     initShips();
@@ -112,42 +113,25 @@ function gameStart() {
         const target=e.currentTarget;
         if(target.classList.contains('rotate'))target.classList.remove('rotate');
             else target.classList.add('rotate');
-
-    }
-    function dragEnd(e) {
-        // e.target.classList.remove('hide');
     }
 
-    function dragEnter(e) {
-        e.preventDefault();
-
-        // get the draggable element
-        const id = elementDragged.id;
-        const draggable = document.getElementById(id);
-        if(!draggable)return;
-        //Check if size fits
-        const curSize = parseInt(draggable.getAttribute('data-size'));
-        const attemptX = parseInt(e.target.getAttribute('data-posX'));
-        const attemptY = parseInt(e.target.getAttribute('data-posY'));
-        const orientation = (draggable.classList.contains('rotate') ? 'v' : 'h');
-        if(orientation==='h' && attemptX<=11-curSize)e.target.classList.add('dragoverAccepted');
-        else if(orientation==='v' && attemptY>=curSize)e.target.classList.add('dragoverAccepted');
-        else e.target.classList.add('dragoverRefused');
-    }
-
-    function dragOver(e) {
+    function dragFunction(e) {
         e.preventDefault();
         // get the draggable element
         const id = elementDragged.id;
         const draggable = document.getElementById(id);
         if(!draggable)return;
-        //Check if size fits
-        const curSize = parseInt(draggable.getAttribute('data-size'));
-        const attemptX = parseInt(e.target.getAttribute('data-posX'));
-        const attemptY = parseInt(e.target.getAttribute('data-posY'));
-        const orientation = (draggable.classList.contains('rotate') ? 'v' : 'h');
-        if(orientation==='h' && attemptX<=11-curSize)e.target.classList.add('dragoverAccepted');
-        else if(orientation==='v' && attemptY>=curSize)e.target.classList.add('dragoverAccepted');
+
+        const curSize = parseInt(elementDragged.getAttribute('data-size'));
+        let attemptX = parseInt(e.target.getAttribute('data-posX'));
+        let attemptY = parseInt(e.target.getAttribute('data-posY'));
+        const orientation = (elementDragged.classList.contains('rotate') ? 'v' : 'h');
+        if(isNaN(attemptX)){
+            attemptX = parseInt(e.target.parentElement.getAttribute('data-posX'));
+            attemptY = parseInt(e.target.parentElement.getAttribute('data-posY'));
+        }   
+        const attempt=currentGame.p1attemptPlacement(curSize,attemptX-1,attemptY-1,orientation);
+        if(attempt)e.target.classList.add('dragoverAccepted');
         else e.target.classList.add('dragoverRefused');
     }
 
@@ -157,25 +141,96 @@ function gameStart() {
     }
 
     function drop(e) {
+        if(elementDragged==='')return;
         e.target.classList.remove('dragoverRefused');
         e.target.classList.remove('dragoverAccepted');
         const curSize = parseInt(elementDragged.getAttribute('data-size'));
         const attemptX = parseInt(e.target.getAttribute('data-posX'));
         const attemptY = parseInt(e.target.getAttribute('data-posY'));
         const orientation = (elementDragged.classList.contains('rotate') ? 'v' : 'h');
-        const attempt=player1Board.placeShip(curSize-1,attemptX-1,attemptY-1,orientation);
+        const attempt=currentGame.p1placeShip(curSize,attemptX-1,attemptY-1,orientation);
         if(!attempt) {
             elementDragged.classList.remove('shipAbs');
+            console.log('placement not allowed');
             return;
+        }
+        else {
+            console.log('placement accepted, new array: ');
+            console.log(currentGame.p1board);
         }
         e.target.appendChild(elementDragged);
         elementDragged.removeEventListener('dragstart', dragStart);
-        elementDragged.removeEventListener('dragend', dragEnd);
         elementDragged.removeEventListener('click', rotate);
+        elementDragged.classList.remove('placeShipHover');
+        elementDragged.classList.add('shipLocked');
+        elementDragged='';
+        //Remove borders of adjacent cells
+        if(curSize>1) {
+            if(orientation==='h') {
+                for(x=0;x<curSize;x++) {
+                    const cellToClean = document.querySelector(`[data-posX="${attemptX+x}"][data-posY="${attemptY}"]`);
+                    cellToClean.classList.remove('boardCell');
+                }
+            }
+            if(orientation==='v') {
+                for(x=0;x<curSize;x++) {
+                    const cellToClean = document.querySelector(`[data-posX="${attemptX}"][data-posY="${attemptY-x}"]`);
+                    cellToClean.classList.remove('boardCell');
+                }
+            }
+        }
         
         //Check if match can start
-        if(player1Board.shipsToPlace<=0) {
+        if(currentGame.readyToAttack===1) {
             console.log('Game can finally start');
+            const boardAI=currentGame.p2board;
+            for(let x=0;x<10;x++) {
+                for(let y=0;y<10;y++) {
+                    const cell=boardAI[x][y];
+                    if(cell!=='') {
+                        const cellToPaint = document.querySelector(`[data-aiX="${x+1}"][data-aiY="${y+1}"]`);
+                        cellToPaint.style.backgroundColor='red';
+                    }
+                }
+            }
+            board2.classList.add('targetCursor');
+            board2.childNodes.forEach(x=> {
+                x.addEventListener('click',shootEnemy);
+            });
+        }
+        function shootEnemy(e) {
+            const target = e.currentTarget;
+            console.log(target);
+            let aiX=parseInt(target.getAttribute('data-aiY'));
+            let aiY=parseInt(target.getAttribute('data-aiX'));
+            aiX--;
+            aiY--;
+            const shot=currentGame.p1attack(aiX, aiY);
+            console.log(shot);
+            if(shot==='missed') {
+                target.style.backgroundImage="url('./assets/splash.png')";
+            }
+            if(shot==='hit') {
+                target.style.backgroundImage="url('./assets/boom.png')";
+            }
+            const enemyAttack=currentGame.attackAI();
+            console.log(enemyAttack.attempt);
+            const x=enemyAttack.rand1;
+            const y=enemyAttack.rand2;
+            const enemyTarget=document.querySelector(`[data-posX="${x+1}"][data-posY="${y+1}"]`);
+            if(enemyAttack.attempt==='missed') {                
+                enemyTarget.style.backgroundImage="url('./assets/splash.png')";
+            }
+            if(enemyAttack.attempt==='hit') {          
+                const explosionDiv = document.createElement('div');
+                explosionDiv.classList.add('explosionDiv');
+                const explosionImg = document.createElement('img');
+                explosionImg.classList.add('explosionImg'); 
+                explosionImg.src='./assets/boom.png';     
+                explosionDiv.appendChild(explosionImg);
+                enemyTarget.style.backgroundImage="url('./assets/boom.png')";
+                enemyTarget.appendChild(explosionDiv);
+            }
         }
     }
 }
--- index.js
+++ index.js
@@ -13,22 +13,47 @@ const Ship = function(len) {
     return {length, hits, sunk, hit, isSunk};
 }
 
-const Gameboard = function() {
+const Gameboard = function(ai=false) {
     const board = [];
+    const boardAttacks = [];
+    let numberOfShips=0;
+    let numberOfSunks=0;
     for(let i=0;i<10;i++) {
         board.push([]);
+        boardAttacks.push([]);
         for(let j=0;j<10;j++) {
             board[i].push('');
+            boardAttacks[i].push('');
         }
     };
-    const shipsToPlace = [1,2,3,4];
+    const shipsToPlace = [1,1,1,2,2,3,3,4];
     function printBoard() {
         console.log(board);
     };
-    function placeShip(n, x, y, orientation='h') {
-        // if(n>=shipsToPlace.length)return false;
-        const len=shipsToPlace[n];
-        
+    function attemptPlacement(len, x, y, orientation='h') {
+        if(isNaN(x) || isNaN(y))return false;
+        //Place horizontally
+        if(orientation==='h' || orientation==='H') {
+            for(let i=0;i<len;i++) {
+                if(x+i>9){console.log('too big');return false;} 
+                if(board[y][x+i]!==''){console.log(board[y][x+i]);return false;}
+                              
+            };
+            return true;
+        }
+        //Place vertically
+        else if(orientation==='v' || orientation==='V') {
+            for(let i=0;i<len;i++) {
+                if(y-i<0)return false;
+                
+                if(board[y-i][x]!=='')return false;
+                                
+            }
+            return true;
+        }
+    }
+    function placeShip(len, x, y, orientation='h') {
+        if(isNaN(x) || isNaN(y))return false;
         //Place horizontally
         if(orientation==='h' || orientation==='H') {
             for(let i=0;i<len;i++) {
@@ -41,59 +66,119 @@ const Gameboard = function() {
                 board[y][x+i] = newShip;
             };
             this.numberOfShips++;
-            this.shipsToPlace.splice(n,1);
+            // this.shipsToPlace--;
+            // this.shipsToPlace.splice(n,1);
             return true;
         }
         //Place vertically
         else if(orientation==='v' || orientation==='V') {
             for(let i=0;i<len;i++) {
-                if(board[y+i][x]!=='')return false;
-                if(y+i>9)return false;                
+                if(y-i<0)return false;
+                if(board[y-i][x]!=='')return false;              
             }
             //Conditions are met, create ship
             const newShip = Ship(len);
             for(let i=0;i<len;i++) {
-                board[y+i][x]= newShip;
+                board[y-i][x]= newShip;
             };
             this.numberOfShips++;
-            this.shipsToPlace.splice(n,1);
+            // this.shipsToPlace--;
+            // this.shipsToPlace.splice(n,1);
             return true;
         }
         
     };
-    function attack(x,y) {
-        if(this.shipsToPlace.length)return false; //Only allow attacks when all ships have been placed
-        if(x<0 || x>9 || y<0 || y>9)return false;
-        if(board[y][x]==='') {
-            board[y][x]='m';
-            return false;
+    function placeAI() {
+        if(!ai)return;
+        const lengthShips=shipsToPlace.length;
+        for(let i=0;i<lengthShips;i++) {
+            while(true) {
+                const rand1=Math.floor(Math.random()*lengthShips);
+                const rand2=Math.floor(Math.random()*lengthShips);
+                const orientation=(Math.random()<0.5) ? 'h' : 'v';
+                if(placeShip(shipsToPlace[i],rand1,rand2,orientation)) {
+                    this.numberOfShips++;
+                    break;
+                }
+            }
         }
-        if(board[y][x]==='m') {
-            return false;
+        console.log(board);
+    }
+    function attack(x,y) {
+        if(this.numberOfShips<shipsToPlace.length)return false; //Only allow attacks when all ships have been placed
+        console.log(x + '   ' + y);
+        if(x<0 || x>9 || y<0 || y>9)return false;
+        if(boardAttacks[y][x]==='h')return false;
+        if(boardAttacks[y][x]==='m')return false;
+        if(board[y][x]==='') {
+            boardAttacks[y][x]='m';
+            return 'missed';
         }
         //The attack has hit
         if(typeof(board[y][x]==='object')) {
             board[y][x].hit();
+            boardAttacks[y][x]='h';
             if(board[y][x].sunk) {
-                this.numberOfShips--;
-                if(this.numberOfShips<=0)return 'Game won';
+                this.numberOfSunks++;
+                if(this.numberOfSunks>=this.numberOfShips)return 'Game over';
                 return 'sunk';
             }
             return 'hit';
         }
     };
+    
     //----
-    return {board, printBoard, shipsToPlace, placeShip, attack};
+    return {board, printBoard, shipsToPlace, numberOfShips, numberOfSunks, attemptPlacement, placeShip, attack, placeAI};
 }
 
-const Game = function(name1, name2, ai=false) {
+const Game = function(name1, name2) {
     const player1 = name1;
     const player2 = name2;
     const player1Board = Gameboard();
-    const player2Board = Gameboard();
+    const player2Board = Gameboard(true);
+    let p1ready = false;
+    let p2ready = false;
+    let readyToAttack=0;
+    let gameOver = false;
+    const p1board=player1Board.board;
+    const p2board=player2Board.board;
+    const p1shipsToPlace=player1Board.shipsToPlace;
+    const p2shipsToPlace=player2Board.shipsToPlace;
+    const p1attemptPlacement = player1Board.attemptPlacement;
+    const p2attemptPlacement = player2Board.attemptPlacement;
+    const p1placeShip = function(...args){
+        const attempt=player1Board.placeShip(...args);
+        if(attempt && player1Board.numberOfShips>=player1Board.shipsToPlace.length) {
+            this.p1ready=true;
+            this.readyToAttack=1;
+            player2Board.placeAI();
+        }
+        return attempt;
+    };
+    const p1attack = function(...args) {
+        const attempt=player2Board.attack(...args);
+        if(!attempt)return false;
+        this.readyToAttack=2;
+        if(attempt==='Game over'){
+            this.gameOver=true;
+            return attempt;
+        }
+        return attempt;
+    };
+    function attackAI() {
+        while(true) {
+            const rand1=Math.floor(Math.random()*10);
+            const rand2=Math.floor(Math.random()*10);
+            const attempt=player1Board.attack(rand1, rand2);
+            if(attempt)return {attempt, rand1, rand2};
+        }
+    }
+
+    //----
+    return {player1, player2, p1board, p2board, p1shipsToPlace, p2shipsToPlace, p1attemptPlacement, p2attemptPlacement, p1placeShip, p1attack, readyToAttack, p1ready, p2ready, attackAI, gameOver};
 }
 
-const myBoard = Gameboard();
+//const myBoard = Gameboard();
 // myBoard.printBoard();
 // myBoard.shipsToPlace(3,3,3,"h");
 // myBoard.printBoard();
